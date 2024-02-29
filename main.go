a24a78fdfa206408a6c9ce43df527c4db63a20c3
DongKeun94 <donggen94@pukyong.ac.kr>
2024-02-29 16:24:07 +0900

    source change

--- main.go
+++ main.go
@@ -30,6 +30,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
-	fmt.Fprintf(w, "TecACe GX !!!\n")
-	fmt.Fprintf(w, "Image modified test !!!\n")
+	fmt.Fprintf(w, "TecACe GX  GX GX GX!!!\n")
+	fmt.Fprintf(w, "Image modified test test test set !!!!!!!!!! !!!\n")
 }
