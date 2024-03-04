5e1af2ac6fb5828e117f6a498b88d6096b2481d5
DongKeun94 <donggen94@pukyong.ac.kr>
2024-03-04 16:20:31 +0900

    test test

--- main.go
+++ main.go
@@ -30,6 +30,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world! imageUpdater\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
-	fmt.Fprintf(w, "TecACe GX  GX GX GX!!!\n")
+	fmt.Fprintf(w, "DKDKDKDKDK DK DK DK KDKKDK KD KDK DK DK KD KDKD KD!!!\n")
 	fmt.Fprintf(w, "Image modified test test test set !!!!!!!!!! !!!\n")
 }
